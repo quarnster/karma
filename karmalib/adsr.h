d1e963b9dce46f00a168dead066d9d1f4b824629
Fredrik Ehnbom <quarnster@gmail.com>
2003-12-25 12:45:12 +0000

    moved extern C-stuff up, new line at eof

--- karmalib/adsr.h
+++ karmalib/adsr.h
@@ -1,6 +1,10 @@
 #ifndef __INCLUDED_KARMA_ADSR_H
 #define __INCLUDED_KARMA_ADSR_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct {
 	int attack;
 	int decay;
@@ -8,13 +12,10 @@ typedef struct {
 	int release;
 } karma_ADSR;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 int karma_ADSR_getValue(karma_ADSR *adsr, long samplepos, long relSample);
 
 #ifdef __cplusplus
 }
 #endif
 #endif
+
