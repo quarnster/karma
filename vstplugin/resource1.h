23ffb896bad6ee434759521dd6d8e1bf38591eb8
Fredrik Ehnbom <quarnster@gmail.com>
2004-01-01 15:44:30 +0000

    added

---
+++ vstplugin/resource1.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by vstplugin.rc
+//
+#define IDB_BITMAP1                     111
+#define IDB_BITMAP2                     112
+#define IDB_BITMAP3                     113
+#define IDB_BITMAP4                     114
+#define IDB_BITMAP5                     115
+#define IDB_BITMAP6                     116
+#define IDB_BITMAP7                     117
+#define IDB_BITMAP8                     118
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        120
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
